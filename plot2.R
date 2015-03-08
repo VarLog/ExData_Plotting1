78d1d9993543006a617cb5be89d3006de659d82e
Maxim Fedorenko <varlllog@gmail.com>
2015-03-08 22:10:47 +0300

    The prototype of second plot was added.

---
+++ plot2.R
@@ -0,0 +1,17 @@
+#!/usr/bin/env R
+
+library(data.table)
+
+# read
+data <- fread("./household_power_consumption.txt", na.strings = c("?"), colClasses = rep("character",9))
+
+# subset
+data[,Date:=as.Date(Date, format="%d/%m/%Y")]
+data <- data[Date>=as.Date("2007-02-01") & Date<=as.Date("2007-02-02")]
+
+# prepare
+data[,Global_active_power:=as.numeric(Global_active_power)]
+data[,datetime:=data.frame(strptime( paste( Date, Time ), format="%Y-%m-%d %H:%M:%S" ))]
+
+# plot
+plot(data$datetime, data$Global_active_power, type="l")
